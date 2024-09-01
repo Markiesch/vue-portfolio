5374e1768bff382bdd02a099bbc93a1bd9917b55
Markiesch <marksmeel@gmail.com>
2024-09-01 22:33:04 +0200

    Feat: added font loader

---
+++ src/components/router-head/router-head.fonts.tsx
@@ -0,0 +1,72 @@
+import { component$ } from "@builder.io/qwik";
+
+export const RouterHeadFonts = component$(() => {
+  return (
+    <>
+      <link
+        rel="preload"
+        href="/fonts/Poppins-Bold.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+
+      <link
+        rel="preload"
+        href="/fonts/Poppins-Italic.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-Regular.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-Medium.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-MediumItalic.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-SemiBold.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-SemiBoldItalic.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-Bold.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins-BoldItalic.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin=""
+      />
+    </>
+  );
+});
