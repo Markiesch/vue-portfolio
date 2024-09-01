dd2eb433ede73b9ffcb39ade720d850f201ccd09
Markiesch <marksmeel@gmail.com>
2024-09-01 22:32:54 +0200

    Feat: improved hero section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,36 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 import { SendMailIcon } from "~/components/icon/send-mail-icon";
 import { ProjectCard } from "~/components/project-card";
 import { component$ } from "@builder.io/qwik";
 import { Button } from "~/components/ui/button";
 import { RainbowGradient } from "~/components/ui/rainbow-gradient";
+import { CodeIcon } from "~/components/icon/code-icon";
+import { DesignIcon } from "~/components/icon/design-icon";
 
 export default component$(() => {
   return (
     <>
       {/* HERO SECTION */}
-
-      <div class="text-center">
-        <h1>Design. Build. Improve.</h1>
-        <h2 class="mx-auto max-w-[16ch] text-7xl font-bold leading-[0.95]">
+      <div class="pb-8 pt-8 text-center">
+        <div class="mx-auto mb-6 w-fit rounded-full border border-secondary bg-secondary px-4 py-2 font-mono">
+          Design. Build. Improve.
+        </div>
+        <h1 class="mx-auto max-w-[16ch] text-7xl font-bold leading-[0.95]">
           I build websites your users will love
-        </h2>
-        <p class="mx-auto max-w-[42ch] pt-8 text-2xl text-foreground-muted">
-          Hi I am Mark, a Software Developer with an eye for UI design. I am
-          specialized in UI/UX design and responsive web development.
+        </h1>
+        <p class="mx-auto max-w-[42ch] pt-8 text-xl text-foreground-muted">
+          Hi I am Mark, a{" "}
+          {/* TODO link to projects related to software development */}
+          <Link href="/" class="text-orange-300 text-semibold">
+            <CodeIcon class="text-orange-300 inline w-5" /> Software Developer
+          </Link>{" "}
+          with an eye for
+          {/* TODO link to projects related to UI design */}
+          <Link href="/" class="text-blue-500 font-semibold">
+            <DesignIcon class="inline w-5" /> UI design
+          </Link>
+          . I am specialized in UI/UX design and responsive web development.
         </p>
 
         <div class="mt-6">
